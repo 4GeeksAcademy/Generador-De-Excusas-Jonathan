d6cab28a62989f3599f09c5819f41171b696c662
Alessander Leon <alessanderleon@gmail.com>
2025-02-18 01:16:21 +0000

    Proyecto terminado, con todo lo requerido

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorDeExcusas() {
+    let who = ['Un dinosaurio', 'Mi gato', 'Mi jefe', 'La materia oscura'];
+    let action = ['destruyo', 'devoro', 'se trago', 'pulverizo'];
+    let what = ['mi trabajo', 'mi computadora', 'tu bien mas preciado', 'tus sueños'];
+    let when = ['antes del trabajo', 'cuando estaba durmiendo', 'mientras me estaba en la ducha', 'durante mi almuerzo'];
+
+    let text1 = who[Math.floor(Math.random() * who.length)]
+    let text2 = action[Math.floor(Math.random() * action.length)]
+    let text3 = what[Math.floor(Math.random() * what.length)]
+    let text4 = when[Math.floor(Math.random() * when.length)]
+
+    let resultado = text1 + " "  + text2 + " " + text3 + " " + text4;
+    
+    return document.getElementById("texto").innerHTML = resultado
+  }
+
+  document.getElementById("boton").onclick = function (){
+    generadorDeExcusas()
+  }
 };
+
+
+
+
+
